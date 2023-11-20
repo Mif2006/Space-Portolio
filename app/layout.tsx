21d231aecd53ca172395fa2e03678b0aa8f7e14b
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-11-19 22:33:23 -0300

    Update layout className to include max width

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden max-w-[1855px] mx-auto`}
       >
         <StarsCanvas />
         <Navbar />
