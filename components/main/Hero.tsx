60b7ff826a34b786cc5c0ec6cafe05bb4f4b256b
Baturalp Güvenç <cloudmelon74@gmail.com>
2023-12-03 19:01:53 +0300

    frontend
    svg folders, logos, homepage and navbar was changed

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -4,13 +4,14 @@ import HeroContent from "../sub/HeroContent";
 const Hero = () => {
   return (
     <div className="relative flex flex-col h-full w-full" id="about-me">
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-grad1 via-grad2 to-grad3" />
       <video
         autoPlay
         muted
         loop
         className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
       >
-        <source src="/blackhole.webm" type="video/webm" />
+        <source src="/" type="video/webm" />
       </video>
       <HeroContent />
     </div>
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+        Examine Chatbot
           </h1>
         </motion.div>
 
@@ -33,12 +33,13 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+          Imagine with 
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Atlaspad Crypto {" "}
             </span>
-            project exprience
+            Launchpad {" "}
+            
           </span>
         </motion.div>
 
@@ -46,8 +47,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          Discover a vibrant community of creators on Serendipity! Connect with thousands of like-minded individuals on Discord or the web and unleash your imagination through collaborative storytelling. From vivid worlds to unforgettable characters, bring your short text descriptions to life in new and exciting ways. Join Serendipity today and embark on a journey of endless creativity!
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
@@ -58,14 +58,15 @@ const HeroContent = () => {
       </div>
 
       <motion.div
-        variants={slideInFromRight(0.8)}
+        variants={slideInFromRight(0.9)}
         className="w-full h-full flex justify-center items-center"
       >
         <Image
-          src="/mainIconsdark.svg"
+          src="/atlas.svg"
           alt="work icons"
-          height={650}
-          width={650}
+          height={850}
+          width={850}
+          className="w-full h-full flex justify-center items-center"
         />
       </motion.div>
     </motion.div>
